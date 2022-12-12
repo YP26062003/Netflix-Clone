b0e77f7fa96eadc3c9a7aea777e27d0c74e00597
YP26062003 <yashapatil2003@gmail.com>
2022-12-12 16:58:09 +0530

    initial commit

---
+++ netflix-clone/src/components/content/Content.js
@@ -0,0 +1,17 @@
+import './Content.css';
+import RowData from '../../constants/URLS.js';
+import Row from '../row/Row';
+
+export default function Content() {
+  return (
+    <div className='content-wrapper'>
+
+      {RowData.map((el) => {
+        return <Row title={el.title} url={el.url} />;
+      })}
+
+      
+    </div>
+  );
+}
+
